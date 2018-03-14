9d37b3d0848e2d37ef3a952494d7dc841c3028e7
Noemi Romero <nlll02@yahoo.com>
2018-03-14 08:36:15 -0500

    initial

---
+++ js/app.js
@@ -0,0 +1,44 @@
+ const a = 4;
+
+ const b = 53;
+ 
+  const c = 57;
+  
+  const d = 16;
+
+  const e = 'Kevin';
+
+  a < b;
+c > d;
+'Name' _=_ 'Name';
+a + b = c;
+a + a >d;
+e = 'Kevin';
+48 != '48';
+
+
+
+
+
+
+while (true) {
+	console.log('Do not run this loop');
+}
+// it is an infinite loop bacause the last exxpression is false so it will execute the code
+const runProgram = true;
+
+while (runProgram) {
+	console.log('Do not run this loop');
+	runProgram = false;
+}
+// this is not an infinite loop because we switch the value
+
+let letters = "A";
+let i = 0;
+
+while (i < 20) {
+	letters += "A";
+	i++;
+}
+
+console.log(letters);
